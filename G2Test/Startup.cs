b496b67e353dc66f51584cb0c592716939fb774d
Jitendra Singh Gariya <jitendra.gariya@ebix.com>
2025-07-18 14:53:49 +0530

    First commit

---
+++ G2Test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(G2Test.Startup))]
+namespace G2Test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
